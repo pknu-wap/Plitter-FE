a02dba27b6700a29c755ae4e7fac455cfef555a0
HB <wods0223@naver.com>
2026-04-11 03:44:07 +0900

    feat: 프로젝트 및 카카오 로그인 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import KakaoLogin from "./pages/KakaoLogin";
+
+
+function App(){
+  return (
+    <div>
+      <h1>Kakao Login Test</h1>
+      <KakaoLogin />
+    </div>
+  );
+}
+
+export default App;
