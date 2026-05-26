70295bf986e744d4dfb09fb708680e589c0565a2
HB <wods0223@naver.com>
2026-05-27 02:59:35 +0900

    design: LP원판 css 적용 #20

--- src/pages/LpPage.jsx
+++ src/pages/LpPage.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import vinylImage from "../assets/lp-vinyl.png";
 import "./LpPage.css";
 
 export default function LpPage() {
@@ -8,15 +9,21 @@ export default function LpPage() {
   return (
     <main className="lp-page">
       <h1>FIND YOUR NUMBER 18</h1>
-    
-      {/* SongSearch에서 받아온 track 있으면 앨범 정보 렌더링 */}
+
+      {/* SongSearch의 track의 앨범 정보 렌더링 */}
       {track && (
         <section className="lp-track-preview">
-          <img
-            className="lp-album-cover"
-            src={track.albumCoverImageUrl}
-            alt={track.title}
-          />
+
+          {/* LP 원판과 앨범 커버 겹치는 박스 */}
+          <div className="lp-record-view">
+            
+            <img className="lp-vinyl" src={vinylImage} alt="" aria-hidden="true" />
+            <img
+              className="lp-album-cover"
+              src={track.albumCoverImageUrl}
+              alt={track.title}
+            />
+          </div>
           <h2>{track.title}</h2>
           <p>{track.artistName}</p>
         </section>
