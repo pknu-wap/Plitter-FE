cb80ed60f5bf6361f31edb118cf141220c24156c
HB <wods0223@naver.com>
2026-05-27 02:00:24 +0900

    feat: LpPage.css 생성 + 앨범 커버 렌더링 #20

--- src/pages/LpPage.jsx
+++ src/pages/LpPage.jsx
@@ -1,17 +1,24 @@
 import { useLocation } from "react-router-dom";
+import "./LpPage.css";
 
 export default function LpPage() {
   const location = useLocation();
   const track = location.state?.track;
 
   return (
-    // SongSearch.css 임시적용
-    <main className="song-search-page">
+    <main className="lp-page">
       <h1>FIND YOUR NUMBER 18</h1>
-
+    
+      {/* SongSearch에서 받아온 track 있으면 앨범 정보 렌더링 */}
       {track && (
-        <section>
-          <p>{track.title}</p>
+        <section className="lp-track-preview">
+          <img
+            className="lp-album-cover"
+            src={track.albumCoverImageUrl}
+            alt={track.title}
+          />
+          <h2>{track.title}</h2>
+          <p>{track.artistName}</p>
         </section>
       )}
     </main>
